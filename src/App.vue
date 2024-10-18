e9309e923df3e67d8e50fe12bb8dfce663efeca9
AubreySL <soligu@outlook.com>
2024-10-18 19:41:12 +0800

    feat: add layout view;

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Description: 
+ * @Author: songlin
+ * @Date: 2024-06-07 16:53:03
+ * @LastEditors: songlin
+ * @LastEditTime: 2024-06-07 16:53:14
+-->
+<template>
+    <LayoutView></LayoutView>
+</template>
+
+<script setup lang="ts">
+import LayoutView from "@/components/layout/index.vue"
+
+</script>
+
+<style scoped></style>
